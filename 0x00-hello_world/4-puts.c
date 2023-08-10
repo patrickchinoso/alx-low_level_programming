e4e366c164f19a4727887848ca12c5a64d5aaae1
patrickchinoso <patrickchinoso@yahoo.com>
2023-08-10 13:32:57 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Heello
+* main - Entry point
 *
-* Return: 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n")
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
