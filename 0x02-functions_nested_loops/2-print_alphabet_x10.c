c327f90bc8adf36b7bfb766a844b500e7c77b719
patrickchinoso <patrickchinoso@yahoo.com>
2023-08-16 06:46:58 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 -print alphabet lower case ten times
+ *Return : 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		_putchar(y);
+	}
+	_putchar('\n');
+}
