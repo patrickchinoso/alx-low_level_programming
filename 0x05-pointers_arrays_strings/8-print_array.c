610d36b1da6ed6cd909a30ce348223a59dd4fdc8
patrickchinoso <patrickchinoso@yahoo.com>
2023-08-21 12:24:39 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements of array to be determined
+ * Return: a and n inputs
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
+}
