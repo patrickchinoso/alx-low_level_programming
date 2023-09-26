c00189ae4e9c4b57fbf0bb26de75835e805c8e61
patrickchinoso <patrickchinoso@yahoo.com>
2023-09-26 12:38:54 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint_safe  function- free a linked list
+ * free_listint_safe - function free a linked list
  * @h: pointer to the first node in the linked list
  * Return: number of elements in the freed list
  */
